4c90cd369b9ffc1fd985fd17ce539c5994567da7
Matheus Gama <mathemovic.mg@gmail.com>
2024-02-17 07:12:04 -0300

    Seccond commit

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,6 +13,12 @@ public class Program {
         cookies.put("phone","984476869");
 
         cookies.remove("email");
+        cookies.put("phone", "981909750");
+
+        System.out.println("Contains 'phone' key: " + cookies.containsKey("phone"));
+        System.out.println("Phone number: " + cookies.get("phone"));
+        System.out.println("Email: " + cookies.get("email"));
+        System.out.println("Size: " + cookies.size());
 
         System.out.println("ALL COOKIES");
         for(String key : cookies.keySet()) {
